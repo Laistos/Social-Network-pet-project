9c4be4c05c1dbd2fa4f47d0f3fcd249cac24c92a
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2023-07-19 00:53:32 +0600

    Add library react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Messages from './components/Messages/Messages';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <Sidebar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <Sidebar />
+        <Routes>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/messages' element={<Messages />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
